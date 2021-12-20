9aa5c0faf314e5a71280d902135a3e847ffa0c29
Tien Do <forever.love.wisdom@gmail.com>
2021-12-21 00:31:13 +0700

    Add Post

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,12 @@
 class BlogController < ApplicationController
   def index
     @blog = Blog.new
+    post1 = @blog.new_post
+    post1.title = "Post 1"
+    post1.body = "Break my own limitations"
+    post1.publish
+    post2=@blog.new_post(title: "Post 2")
+    post2.body="Break my own limitations 2"
+    post2.publish
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,11 +1,28 @@
 class Blog
+  attr_reader :entries
+  attr_writer :post_source
+
   def title
-    "What is my own limitations?"
+    'What is my own limitations?'
   end
 
   def subtitle
-    "Trying to maximize my impact while I am still alive"
-
+    'Trying to maximize my impact while I am still alive'
   end
 
+  def initialize
+    @entries = []
+  end
+
+  def new_post
+    post_source.call.tap do |post|
+      post.blog = self
+    end
+  end
+
+  private
+
+  def post_source
+    @post_source ||= Post.public_method(:new)
+  end
 end
